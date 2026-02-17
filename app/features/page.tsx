1b81f2668146708cd03e460eb5a000b34e997988
saadferozee <saadferozee@gmail.com>
2026-02-17 14:17:25 +0600

    [ADDED] features folder in component directory

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -1,3 +1,5 @@
+import Heading from "@/components/features/Heading";
+
 export default function FeaturesPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-black text-white">
@@ -5,6 +7,7 @@ export default function FeaturesPage() {
         <h1 className="text-4xl font-bold">Features Page</h1>
         <p className="mt-4 text-gray-400">
           This is a placeholder for the Features page.
+          <Heading/>
         </p>
       </div>
     </div>
