b993a45531c3ff744257a8da4203cac141a53ff2
Moksina-Akter <msdiya01865@gmail.com>
2026-02-18 23:50:03 +0600

    [MODIFIED] : updated team section

--- components/about/team/team-card.tsx
+++ components/about/team/team-card.tsx
@@ -4,11 +4,13 @@ import { TeamMember } from "@/components/types.ts";
 export default function TeamCard({ member }: { member: TeamMember }) {
   return (
     <div className="border rounded-2xl p-6 text-center hover:shadow-lg transition">
-      <Avatar className="mx-auto mb-4">
+      <Avatar className="mx-auto  mb-4">
         <AvatarFallback>{member.name[0]}</AvatarFallback>
       </Avatar>
 
-      <h3 className="font-semibold">{member.name}</h3>
+      <h3 className="font-semibold hover:text-cyan-300 text-xl">
+        {member.name}
+      </h3>
       <p className="text-sm text-muted-foreground">{member.role}</p>
     </div>
   );
