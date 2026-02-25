c18e158d1308984f82cd1ac78750ce82825220ce
Tonmoy <tonmoybiswas20602@gmail.com>
2026-02-25 16:36:04 +0600

    [MODIFIED]:FIX BUILD ISSUE

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,5 +1,5 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
