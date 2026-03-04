59f1a1e6dd73f0e1808532f2f69cf380dfc8468c
saadferozee <saadferozee@gmail.com>
2026-03-04 11:27:48 +0600

    [ADDED] create endpoint route for AI response

--- app/(dashboard)/dashboard/keyword-gap-analysis/page.tsx
+++ app/(dashboard)/dashboard/keyword-gap-analysis/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { Separator } from "@/components/ui/separator";
@@ -37,11 +39,39 @@ export default function KeywordGapAnalysisPage() {
                     <div className="ml-auto px-4 flex items-center gap-2">
                         <span className="size-1.5 rounded-full bg-emerald-400 shadow-[0_0_8px_rgba(52,211,153,0.8)] animate-pulse" />
                         <span className="text-[10px] font-mono uppercase tracking-widest text-muted-foreground">
-                            AI Interviewer Online
+                            AI Resume Checker Online
                         </span>
                     </div>
                 </header>
-                
+
+                {/* Main content start */}
+                <div className="p-6 lg:p-8 flex flex-col gap-8">
+                    {/* Page title */}
+                    <div className="flex items-end justify-between">
+                        <div>
+                            <p className="text-[10px] font-mono uppercase tracking-[0.25em] text-primary mb-2">
+                                Resume Improvement
+                            </p>
+                            <h1 className="text-3xl lg:text-4xl font-black tracking-tight text-foreground">
+                                Keyword Gap{" "}
+                                <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary to-blue-500">
+                                    Analysis
+                                </span>
+                            </h1>
+                            <p className="text-muted-foreground text-sm mt-1.5">
+                                Improve your resume with more JD acceptable keywords.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="p-6 lg:p-8">
+                    <div className="flex justify-around gap-5">
+                        {/* Resume input */}
+                        <form className="border w-[50%] min-h-20"></form>
+                        {/* Job Description input */}
+                        <form className="border w-[50%] min-h-20"></form>
+                    </div>
+                </div>
             </SidebarInset>
         </SidebarProvider>
     )
