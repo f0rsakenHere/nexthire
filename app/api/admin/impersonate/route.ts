9b8403f5b1caa6deba2710f24be48ea5eca355bd
Moksina-Akter <msdiya01865@gmail.com>
2026-03-10 20:05:07 +0600

    [ADDED]: connect database inside impersonation api

--- app/api/admin/impersonate/route.ts
+++ app/api/admin/impersonate/route.ts
@@ -1,6 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 import admin from "@/lib/firebase-admin";
+import { connectDB } from "@/lib/mongodb";
 
 export async function POST(req: NextRequest) {
+  try {
+    const { targetUid } = await req.json();
+    const db = await connectDB();
+}
+
+//    catch (error) {
+//     console.error(error);
+//     return NextResponse.json({ error: "Server error" }, { status: 500 });
+//   }
+
   return NextResponse.json({ message: "Impersonation API created" });
 }
